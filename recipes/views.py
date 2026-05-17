c1b9b4788ecab82c1dce52037e9a898d18c37349
Leonardo G.T. da Conceicao <leonardogtc@gmail.com>
2026-05-17 14:28:17 -0300

    Visualização de função render e alteração dos namespaces.

--- recipes/views.py
+++ recipes/views.py
@@ -3,7 +3,9 @@ from django.shortcuts import render
 
 def home(request):
     # This is a placeholder view function
-    return render(request, 'recipes/home.html')
+    return render(request, 'recipes/home.html', context={
+        'name': 'Leonardo',
+    })
 
 
 def sobre(request):
