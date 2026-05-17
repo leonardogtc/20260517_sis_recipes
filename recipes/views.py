a538641e44c6d74257228d42c561ac02cb74eebe
Leonardo G.T. da Conceicao <leonardogtc@gmail.com>
2026-05-17 13:19:45 -0300

    Criação do template Home, alteração de settings indicando pasta global de templates e criação de spaces.

--- recipes/views.py
+++ recipes/views.py
@@ -1,15 +1,14 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def home(request):
     # This is a placeholder view function
-    return HttpResponse("Página principal do seu aplicativo.")
+    return render(request, 'recipes/home.html')
 
 
 def sobre(request):
-    return HttpResponse('sobre.html')
+    return render(request, 'recipes/sobre.html')
 
 
 def contato(request):
-    return HttpResponse('contato.html')
+    return render(request, 'recipes/contato.html')
--- recipesAdmin/settings.py
+++ recipesAdmin/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'recipes',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,9 @@ ROOT_URLCONF = 'recipesAdmin.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'base_templates',
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -102,9 +105,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
