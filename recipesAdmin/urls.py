7d576c0ab714862d81ff653d1515a9586c6eedea
Leonardo G.T. da Conceicao <leonardogtc@gmail.com>
2026-05-17 12:44:56 -0300

    Estudo de request - response exemplificados no urls.py.

--- recipesAdmin/urls.py
+++ recipesAdmin/urls.py
@@ -1,19 +1,3 @@
-"""
-URL configuration for recipesAdmin project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.http import HttpResponse
 from django.urls import path
 from django.contrib import admin
@@ -24,7 +8,17 @@ def my_view(request):
     return HttpResponse("This is the 'sobre' page.")
 
 
+def sobre(request):
+    return HttpResponse(request, 'sobre.html')
+
+
+def contato(request):
+    return HttpResponse(request, 'contato.html')
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('sobre/', my_view, name='sobre'),
+    path('', my_view, name='sobre'),
+    path('sobre/', sobre, name='sobre'),
+    path('contato/', contato, name='sobre'),
 ]
