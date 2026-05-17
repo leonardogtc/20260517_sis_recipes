ee44a8eda311cdef679d8cb608435362ffbacd6f
Leonardo G.T. da Conceicao <leonardogtc@gmail.com>
2026-05-17 18:39:47 -0300

    Introdução aos arquiivos estáticos no Django.

--- recipesAdmin/urls.py
+++ recipesAdmin/urls.py
@@ -1,7 +1,12 @@
 from django.urls import include, path
 from django.contrib import admin
+from django.conf.urls.static import static
+from recipesAdmin import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('recipes.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
