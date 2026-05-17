ed0e0a2bbb8c265dd08336fbcf13e30a7e10478c
Leonardo G.T. da Conceicao <leonardogtc@gmail.com>
2026-05-17 16:28:39 -0300

    Criando a header, separando um "partials" para head e usando include no template.

--- recipes/views.py
+++ recipes/views.py
@@ -3,6 +3,6 @@ from django.shortcuts import render
 
 def home(request):
     # This is a placeholder view function
-    return render(request, 'recipes/home.html', context={
+    return render(request, 'recipes/pages/home.html', context={
         'name': 'Leonardo',
     })
