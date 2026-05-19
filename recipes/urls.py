1847b898c1e5f15507b0136fbbff71610e0dc64c
Leonardo G.T. da Conceicao <leonardogtc@gmail.com>
2026-05-19 08:42:11 -0300

    Nome único nas URLS e app_name para namespace.

--- recipes/urls.py
+++ recipes/urls.py
@@ -2,6 +2,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'recipes'
+
 urlpatterns = [
     path('', views.home, name='home'),
     path('recipe/<int:id>/', views.recipe, name='recipe'),
