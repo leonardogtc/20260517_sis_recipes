e8a683af944b3571f38585936f11a67a6142012f
Leonardo G.T. da Conceicao <leonardogtc@gmail.com>
2026-05-19 04:48:37 -0300

    Injetando dados na lista da view Home utilizando o módulo faker do python.

--- recipes/views.py
+++ recipes/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render
+from utils.recipes.factory import make_recipe
 
 
 def home(request):
     # This is a placeholder view function
     return render(request, 'recipes/pages/home.html', context={
-        'name': 'Leonardo',
+        'recipes': [make_recipe() for _ in range(10)],
     })
 
 
 def recipe(request, id):
     return render(request, 'recipes/pages/recipe-view.html', context={
-        'name': 'Luiz Otávio',
+        'recipe': make_recipe(),
     })
