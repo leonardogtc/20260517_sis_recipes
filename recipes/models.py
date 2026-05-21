3a3bc572ebbe9ac23ca9426fd984f35a43d13bee
Leonardo G.T. da Conceicao <leonardogtc@gmail.com>
2026-05-21 14:15:16 -0300

    Corrigindo category com blank=True e default=None

--- recipes/models.py
+++ recipes/models.py
@@ -29,7 +29,8 @@ class Recipe(models.Model):
     cover = models.ImageField(
         upload_to='recipes/covers/%Y/%m/%d/', blank=True, default='')
     category = models.ForeignKey(
-        Category, on_delete=models.SET_NULL, null=True
+        Category, on_delete=models.SET_NULL, null=True, blank=True,
+        default=None, related_name='Categoria'
     )
     author = models.ForeignKey(
         User, on_delete=models.SET_NULL, null=True
