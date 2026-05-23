4b949694d83b60830274bce4764b0b873c21923e
Leonardo G.T. da Conceição <leonardogtc@gmail.com>
2026-05-23 11:16:42 -0300

    Iniciando com testes no Django com Pytest, Pytest-django e Unittest

--- recipes/tests.py
+++ recipes/tests.py
@@ -1,3 +1,6 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class RecipeURLsTest(TestCase):
+    def test_the_pytest_is_ok(self):
+        self.assertEqual(1, 1, "The pytest is not working properly")
